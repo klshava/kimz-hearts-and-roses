20e7238bc6ab7c3449bb9f066c6faf22178ecd6c
kudzaishava <168266818+kudzaishava@users.noreply.github.com>
2026-04-24 07:44:52 +1000

    v1.1 - updated base

--- kimz-hearts-roses/vite.config.ts
+++ kimz-hearts-roses/vite.config.ts
@@ -207,6 +207,7 @@ const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(
 
 export default defineConfig({
   plugins,
+  base: "/kimz-hearts-and-roses/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
